40b10f17fe5f5be9aee26dcfc03fc61f9078e87c
Techharik <khariprasath30@gmail.com>
2024-01-02 21:42:26 +0530

    basic layout is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
+import CategoryItem from './components/category-item/category-item.component';
 
-const App = () =>{
+import './categories.styles.scss';
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+    },
+  ];
+
   return (
-   <div>
-    <div class= 'catogeries-container'>
-      
+    <div className='categories-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
     </div>
-   </div>
   );
-}
+};
 
 export default App;
