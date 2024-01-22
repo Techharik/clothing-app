c335cf38e28c6a64962dafabe7d5e84938f5f557
Techharik <khariprasath30@gmail.com>
2024-01-22 13:30:37 +0530

    adding styled components

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Link } from 'react-router-dom';
-import './navigation.styles.scss';
+import {NavigationContainer , LogoContainer , NavLinks , NavLink} from'./navigation.styles.jsx';
 import { useContext } from 'react';
 import { userContext } from '../../context/user.context';
 import { signOutUser } from '../../utilits/firebase.utilits';
@@ -19,29 +19,29 @@ const Navigation = () => {
 
   return (
     <>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
+      <NavigationContainer >
+        <LogoContainer className='logo-container' to='/'>
           {/* <CrwnLogo className='logo' /> */}
-        </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        </LogoContainer>
+        <NavLinks className='nav-links-container'>
+          <NavLink className='nav-link' to='/shop'>
             SHOP
-          </Link>
-          <Link className='nav-link' to='/sign-in'>
+          </NavLink>
+          <NavLink className='nav-link' to='/sign-in'>
             {currentUser?
-            <span className="nav-link" onClick={siginOutHandler}>
+            <NavLink as={'span'}  onClick={siginOutHandler}>
               Sign Out
-            </span>
+            </NavLink>
           :
-            <span className="nav-link">
+            <NavLink as={'span'} >
               Sign In
-            </span>
+            </NavLink>
           }
-          </Link>
+          </NavLink>
         <CartIcon />
        {isCartOpen &&  <CartDropdown />}
-        </div>
-      </div>
+        </NavLinks>
+      </NavigationContainer>
       <Outlet />
     </>
   )
